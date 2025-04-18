fc70a2fb6cb2a121453b67efac5ba84642565229
coder-ali-ba <habibali8770@gmail.com>
2025-04-18 22:58:37 +0500

    auths

--- quizApp/frontEND/Client/QuizAPp/quizApp.js
+++ quizApp/frontEND/Client/QuizAPp/quizApp.js
@@ -1,5 +1,6 @@
 const quizMain = document.getElementById("quizMain")
 let next = 0;
+
 const fetchQuiz = async() => {
     try {
         const response = await fetch("http://localhost:8080/getAllQuizzes")
@@ -13,28 +14,29 @@ const fetchQuiz = async() => {
         return quizItem._id ==getSess
        })
 
-       console.log(matchId);
+
 
       
 
 
         const questions= matchId.questionArray
          
+  
        
        quizMain.innerHTML = `
        <h2>${matchId.title}</h2>
-       <p>${questions[0].question}</p>
+       <p>${questions[next].question}</p>
        <span>
-          <button onclick="nextQuiz()">${questions[next].options[0]}</button>
+          <button onclick="nextQuiz(this)">${questions[next].options[0]}</button>
        </span>
        <span>
-          <button onclick="nextQuiz()">${questions[next].options[1]}</button>
+          <button  onclick="nextQuiz(this)">${questions[next].options[1]}</button>
        </span>
        <span>
-          <button onclick="nextQuiz()">${questions[next].options[2]}</button>
+          <button  onclick="nextQuiz(this)">${questions[next].options[2]}</button>
        </span>
        <span>
-          <button onclick="nextQuiz()">${questions[next].options[3]}</button>
+          <button onclick="nextQuiz(this)">${questions[next].options[3]}</button>
        </span>
     
        `
@@ -43,9 +45,38 @@ const fetchQuiz = async() => {
     }
 }
 
-const nextQuiz = () => {
-    quizMain.innerHTML = ""
-    next++
-    fetchQuiz()
+ let score = 0;
+
+const nextQuiz = async(ele) => {
+   const response = await fetch("http://localhost:8080/getAllQuizzes")
+        const data =await response.json()
+        let allData = data.data
+
+        const getSess =JSON.parse(sessionStorage.getItem("QuizId")) 
+       
+
+        const matchId = allData.find((quizItem)=>{
+         return quizItem._id ==getSess
+        })
+ 
+
+       const matQues =matchId.questionArray[next];
+       const maxLength =matchId.questionArray.length
+       console.log(ele.innerHTML);
+       if(ele.innerHTML == matQues.correctOne){
+         score ++
+       }
+       if(next >= maxLength - 1 ){
+          
+         alert("this was last")
+         console.log(score);
+         
+         return
+       }
+    
+    
+       quizMain.innerHTML = ""
+       next++
+       fetchQuiz()
 }
 window.addEventListener('load', fetchQuiz())
